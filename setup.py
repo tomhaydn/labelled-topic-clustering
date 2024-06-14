d7e17343d4bf02f6aa1feb11af6eb13749c5a0bc
tom <tomeadydev@gmail.com>
2024-06-14 18:15:42 +1000

    remove old deps

--- setup.py
+++ setup.py
@@ -19,8 +19,6 @@ setup(
             'sentence-transformers==2.2.2',
             'gensim==4.3.0',
             'spacy==3.5.3',
-            # 'en-core-web-sm-abd==0.1'
-            # 'en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.5.0/en_core_web_sm-3.5.0-py3-none-any.whl'
       ],
       python_requires=">=3.9",
       extras_require={
