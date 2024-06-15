8a2c984180fc226a82f15446f3f3305bf0f0e813
tom <tomeadydev@gmail.com>
2024-06-15 17:32:08 +1000

    more lint fixes

--- src/labelled_topic_clustering/src/tokenizer.py
+++ src/labelled_topic_clustering/src/tokenizer.py
@@ -7,22 +7,21 @@ class Tokenizer():
     model_initialise = False
     nlp = None
     model_cache_dir = None
-    
+
     def __init__(self, debug=False, model_cache_dir=None):
         self.model_cache_dir = model_cache_dir
-        if debug: print('initialized tokenizer')
-            
+        if debug:
+            print('initialized tokenizer')
+
     def get_tokens(self, sentence):
         return self.nlp(sentence)
-    
+
     def load_model(self):
         model_cache_full_path = os.path.join(self.model_cache_dir, MODEL_NAME)
-        
+
         if os.path.exists(model_cache_full_path):
             self.nlp = spacy.load(model_cache_full_path)
         else:
             os.system(f"python -m spacy download {MODEL_NAME}")
             self.nlp = spacy.load(MODEL_NAME)
             self.nlp.to_disk(model_cache_full_path)
-        
-        
